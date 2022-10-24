24978864b6469302839e9568e1e1316179feea81
BT_3W2_13 <Kellyx8@nycstudents.net>
2022-10-24 09:07:33 -0400

    changes

--- src/LinearEquationRunner.java
+++ src/LinearEquationRunner.java
@@ -3,6 +3,7 @@ public class LinearEquationRunner {
     public static void main (String[] args) {
         Scanner input = new Scanner(System.in);
 
+
         System.out.println("Welcome!");
         System.out.print("Enter coordinate 1: ");
         String cord1 = (input.nextLine());
@@ -11,14 +12,15 @@ public class LinearEquationRunner {
         System.out.println();
         int Comma = cord1.indexOf(",");
         String X1 = cord1.substring(1,Comma);
-        double c1 = Double.parseDouble(X1);
+        int c1 = Integer.parseInt(X1);
         String Y1 = cord1.substring(Comma + 1, cord1.length()-1);
-        double c2 = Double.parseDouble(Y1);
+        int c2 = Integer.parseInt(Y1);
         String X2 = cord2.substring(1,Comma);
-        double c3 = Double.parseDouble(X2);
+        int c3 = Integer.parseInt(X2);
         String Y2 = cord2.substring(Comma + 1, cord1.length()-1);
-        double c4 = Double.parseDouble(Y2);
+        int c4 = Integer.parseInt(Y2);
         LinearEquation Cords = new LinearEquation (c1,c2,c3,c4);
+        System.out.print
 
 
 
