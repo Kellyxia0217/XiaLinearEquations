8c2738df4f5621e3909ffc25210d42a21cd84445
Kellyxia0217 <114585624+Kellyxia0217@users.noreply.github.com>
2022-11-01 18:40:37 -0400

    Update LinearEquation.java

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,50 +1,47 @@
-public class LinearEquation {
-    private int x1;
-    private int x2;
     private int y1;
     private int y2;
 
-   public LinearEquation (int x1, int y1, int x2, int y2) {
-       this.x1 = x1;
-       this.y1 = y1;
-       this.x2 = x2;
-       this.y2 = y2;
-
+    public LinearEquation(int x1, int y1, int x2, int y2) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
 
 
     }
 
     public double getDistance() {
-       return Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2));
-    }
-    public double getSlope()  {
-       return ((double)(y2-y1)/(x2-x1));
-
-
-
-
-    }
-    public double getyintercept()  {
-       return  (y1-(((y2-y1)/(x2-x1))*x1));
+        return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
     }
 
+    public double getSlope() {
+        return ((double) (y2 - y1 / x2 - x1));
 
+    }
 
+    public double getyintercept() {
+        return ((double)y1-(getSlope() * x1)) ;
+    }
 
 
     public String EquationOfLine() {
-       return "y= " + ("" + (y2-y1) + "/" + (x2-x1) + "x") + "+" + getyintercept();
+        return "y = " + ("" + (y2 - y1) + "/" + (x2 - x1) + "x") + "+" +  getyintercept();
 
+    }
+    public double Round(double num) {
+       return Math.round(num*100.0)/100.0;
+    }
+    public double Newpoint(double num) {
+        return (getSlope() * num) + getyintercept();
     }
 
 
+
     public void LineInfo() {
-       System.out.println("The two points are:  " + "(" + x1 + "," + y1 + ") " + "and" +  "(" + x2 + "," + y2 + ") ");
-       System.out.println("The equation of the line between these points is: " + EquationOfLine());
-       System.out.println("The slope of this line is: " + getSlope());
-       System.out.println("The y-intercept of the line is: " + getyintercept());
-       System.out.println("The distance of the line is: " + getDistance());
-
-
+        System.out.println("The two points are:  " + "(" + x1 + "," + y1 + ") " + "and" + "(" + x2 + "," + y2 + ") ");
+        System.out.println("The equation of the line between these points is: " + EquationOfLine());
+        System.out.println("The slope of this line is: " + getSlope());
+        System.out.println("The y-intercept of the line is: " + getyintercept());
+        System.out.println("The distance of the line is: " + Round(getDistance()));
     }
 }
